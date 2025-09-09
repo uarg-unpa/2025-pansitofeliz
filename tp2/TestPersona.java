ab77cdc9e092a5d9200759e6d9e31ba55327c47e
Magali Bulnes <148983361+pansitofeliz@users.noreply.github.com>
2025-09-09 20:33:23 -0300

    actividad test persona
    buenas noches, tengo un problema. escribí el código como lo pasaron en discord pero no puedo enlazar Domicilio con Persona y TestPersona, sale como que Domilicio no esta definido, estuve probando y no pude llegar a arreglarlo.

---
+++ tp2/TestPersona.java
@@ -0,0 +1,14 @@
+package tp2;
+
+public class TestPersona {
+    public static void main(String[] args) {
+        Domicilio dom1 = new Domicilio("rivadavia", 1234);
+        Persona p1 = new Persona("Laura", 20, "12345678", 'M', 60.5, 1.65, dom1);
+        System.out.println(p1);
+        System.out.println(p1.infoDomicilio());
+
+        Persona p2 = new Persona("Pedro");
+        System.out.println(p2); // Domicilio por defecto
+    }
+}
+
